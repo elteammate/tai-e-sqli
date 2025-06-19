c2ad07c5351d9f6fe3659a94ffb3729036750c09
elteammate <elteammate@gmail.com>
2025-06-19 21:56:53 +0300

    Use the latest version of Tai-e

--- run_tai-e-test.sh
+++ run_tai-e-test.sh
@@ -4,7 +4,7 @@ mkdir -p /tmp/classes &&
 /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/javac Main.java -d /tmp/classes &&
 ./gradlew :fatJar &&
 java \
-  -jar build/tai-e-all-0.5.1.jar \
+  -jar build/tai-e-all-0.5.2-SNAPSHOT.jar \
   -a "pta=cs:2-type;distinguish-string-constants:all;merge-string-objects:false;merge-string-builders:false;plugins:[org.example.TestExtraEntryPoints];taint-config:taint-config;taint-config-providers:[org.example.TestConfigProvider]" \
   -a "ir-dumper" \
   -java 8 \
